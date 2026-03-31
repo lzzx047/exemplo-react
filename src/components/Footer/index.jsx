10ecb1002e866c0bbf8c2e010efb1c3f73e9196a
Luiz <luizmarquesfernando037@gmail.com>
2026-03-30 22:14:35 -0300

    exemplo react renan

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+function Footer() {
+
+    return (
+
+        <footer id="footer">
+      <p> Minha primeira API React - &copy; 
+        Todos os meus direitos reservados </p>
+    </footer>
+
+    )
+
+}
+
+export default Footer 
---
+++ src/components/List/index.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+
+function List() {
+    return (
+        
+    <section>
+      <h2 id="exemplo-h2" >Seção de listas</h2>
+
+      <h3>Lista não ordenada</h3>
+      <ul id="ul">
+        <li>batata</li>
+        <li>Pão</li>
+        <li>Arroz</li>
+      </ul>
+
+      <h3>Lista ordenada</h3>
+        <ol>
+        <li>brites</li>
+        <li>Luiz</li>
+        <li>Kauan</li>
+      </ol>
+
+    </section>
+    )
+}
+export default List
